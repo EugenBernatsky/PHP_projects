9a56f6bef9d57944270400fa5712099d9ce7407d
EugenBernatsky <114477328+EugenBernatsky@users.noreply.github.com>
2025-12-10 19:33:55 +0200

    Add files via upload

---
+++ 16_Project_diary_(php)/Final/inc/db-connect.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;port=3307;dbname=diary;charset=utf8mb4', 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+}
+
+catch (PDOException $e) {
+    var_dump($e->getMessage());
+    echo 'A problem occured with the database connection...';
+    die();
+}
